938ce60694dec68a0e8dd0707008aa8842acc3ef
Andra-sun <111245270+Andra-sun@users.noreply.github.com>
2023-07-05 13:33:04 -0300

    Update charts.js

--- charts.js
+++ charts.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         datasets: [
             {
                 label: "Crescimento da IA",
-                data: [10, 20, 30, 40, 5, 60],
+                data: [10, 20, 30, 40, 50, 60],
                 backgroundColor: "rgba(75, 192, 192, 0.2)", // Cor de fundo do gráfico
                 borderColor: "rgba(75, 192, 192, 1)", // Cor da linha do gráfico
                 borderWidth: 2, // Largura da linha do gráfico
