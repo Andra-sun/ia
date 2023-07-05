eb3937b8e8d565001837fba61ca159cd0ce2897a
Andra-sun <111245270+Andra-sun@users.noreply.github.com>
2023-07-05 18:42:05 -0300

    Add files via upload

--- charts.js
+++ charts.js
@@ -4,13 +4,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Defina os dados do gráfico
     var data = {
-        labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho"],
+        labels: [
+            "Janeiro",
+            "Fevereiro",
+            "Março",
+            "Abril",
+            "Maio",
+            "Junho",
+            "Julho",
+            "Agosto",
+        ],
         datasets: [
             {
-                label: "Crescimento da IA",
-                data: [10, 20, 30, 40, 50, 60],
+                label: "informação A",
+                data: [30, 20, 30, 40, 5, 60, 10, 100],
                 backgroundColor: "rgba(75, 192, 192, 0.2)", // Cor de fundo do gráfico
                 borderColor: "rgba(75, 192, 192, 1)", // Cor da linha do gráfico
+                borderWidth: 2, // Largura da linha do gráfico
+            },
+            {
+                label: "informação B",
+                data: [4, 25, 0, 40, 5, 6, 10, 20],
+                backgroundColor: "rgba(75, 192, 192, 0.2)", // Cor de fundo do gráfico
+                borderColor: "rgba(5, 192, 92, 1)", // Cor da linha do gráfico
                 borderWidth: 2, // Largura da linha do gráfico
             },
         ],
