a4b3128fd10b8875366f0a6bdcef90e005f49a52
Andra-sun <111245270+Andra-sun@users.noreply.github.com>
2023-07-22 16:49:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,45 @@
 // Obtém todas as caixas azuis com a classe "application-item"
 const boxes = document.querySelectorAll(".application-item");
 
-// Itera sobre todas as caixas azuis e adiciona um evento de clique a cada uma
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        // Obtém o parágrafo dentro da caixa azul clicada
         const paragraph = box.querySelector(".application-description");
-
-        // Verifica se o parágrafo está visível
         const isVisible = paragraph.style.display === "block";
 
         if (isVisible) {
-            // Se o parágrafo estiver visível, oculta-o e redefine a altura e largura da caixa azul
             paragraph.style.display = "none";
-            box.style.height = "4rem"; // Defina a altura desejada inicialmente
-            box.style.width = "10.3rem"; // Defina a largura desejada inicialmente
+            box.style.height = "4rem";
+            box.style.width = "10.3rem";
+            box.classList.remove("description-visible");
         } else {
-            // Se o parágrafo estiver oculto, exibe-o e expande a caixa azul
             paragraph.style.display = "block";
             box.style.height = "auto";
-            box.style.width = "auto"; // Define a largura como 100% para ocupar todo o espaço disponível
+            box.style.width = "auto";
+            box.classList.add("description-visible");
         }
     });
 });
 
-// Obtém todas as caixas azuis com a classe "application-item"
+// Obtém todas as caixas azuis com a classe "ruim-application-item"
 const boxesRuim = document.querySelectorAll(".ruim-application-item");
 
-// Itera sobre todas as caixas azuis-escuras e adiciona um evento de clique a cada uma
 boxesRuim.forEach((boxr) => {
     boxr.addEventListener("click", () => {
-        // Obtém o parágrafo dentro da caixa azul clicada
         const ruimparagraph = boxr.querySelector(
             ".ruim-application-description"
         );
-
-        // Verifica se o parágrafo está visível
         const ruimisVisible = ruimparagraph.style.display === "block";
 
         if (ruimisVisible) {
-            // Se o parágrafo estiver visível, oculta-o e redefine a altura e largura da caixa azul
             ruimparagraph.style.display = "none";
-            boxr.style.height = "4rem"; // Defina a altura desejada inicialmente
-            boxr.style.width = "10.3rem"; // Defina a largura desejada inicialmente
+            boxr.style.height = "4rem";
+            boxr.style.width = "10.3rem";
+            boxr.classList.remove("description-visible"); // Remove the class when description is hidden
         } else {
-            // Se o parágrafo estiver oculto, exibe-o e expande a caixa azul
             ruimparagraph.style.display = "block";
             boxr.style.height = "auto";
-            boxr.style.width = "auto"; // Define a largura como 100% para ocupar todo o espaço disponível
+            boxr.style.width = "auto";
+            boxr.classList.add("description-visible"); // Add the class when description is shown
         }
     });
 });
@@ -64,15 +56,14 @@ boxesDiv.forEach((boxDiv) => {
         if (isParagraphVisible) {
             dvParagraph.style.display = "none";
             boxDiv.style.width = "19rem";
-            boxDiv.style.margin = "1.5rem"
+            boxDiv.style.margin = "0.5rem";
             dvImage.style.marginTop = "1rem";
         } else {
             dvParagraph.style.display = "block";
             dvImage.style.marginTop = "-3rem";
             boxDiv.style.height = "auto";
             boxDiv.style.width = "19rem";
-            boxDiv.style.margin = "2.5rem"
-
+            boxDiv.style.margin = "2.5rem";
         }
     });
 });
@@ -80,25 +71,23 @@ boxesDiv.forEach((boxDiv) => {
 // Obtém todas as caixas azuis com a classe "pedro-application-item"
 const boxesPedro = document.querySelectorAll(".pedro-application-item");
 
-// Itera sobre todas as caixas azuis-escuras e adiciona um evento de clique a cada uma
 boxesPedro.forEach((item) => {
     item.addEventListener("click", () => {
-        // Obtém o parágrafo dentro da caixa azul clicada
-        const description = item.querySelector(".pedro-application-description");
-
-        // Verifica se o parágrafo está visível
+        const description = item.querySelector(
+            ".pedro-application-description"
+        );
         const isVisible = description.style.display === "block";
 
         if (isVisible) {
-            // Se o parágrafo estiver visível, oculta-o e redefine a altura e largura da caixa azul
             description.style.display = "none";
-            item.style.height = "4rem"; // Defina a altura desejada inicialmente
-            item.style.width = "15rem"; // Defina a largura desejada inicialmente
+            item.style.height = "4rem";
+            item.style.width = "15rem";
+            item.classList.remove("description-visible"); // Remove the class when description is hidden
         } else {
-            // Se o parágrafo estiver oculto, exibe-o e expande a caixa azul
             description.style.display = "block";
             item.style.height = "auto";
-            item.style.width = "auto"; // Define a largura como 100% para ocupar todo o espaço disponível
+            item.style.width = "auto";
+            item.classList.add("description-visible"); // Add the class when description is shown
         }
     });
 });
